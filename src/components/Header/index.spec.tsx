71025f29b54e36930903aa75a9730b02042f58c7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-17 16:51:41 -0300

    :test_tube: test: adiciona testes de renderização do componente Header
    Cria testes para garantir que o componente Header é renderizado
    corretamente e aplica a classe CSS esperada.

---
+++ src/components/Header/index.spec.tsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react'
+import Header from './index'
+
+describe('Header', () => {
+    test('deveria renderizar o componente', () => {
+        expect(<Header>Meu Header</Header>).toBeTruthy()
+    })
+
+    test('deveria renderizar o componente com a classe correta', () => {
+        const { container } = render(<Header>Meu Header</Header>)
+
+        expect(container.firstChild).toHaveClass('header')
+    })
+})
