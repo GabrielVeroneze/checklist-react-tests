f20e520c2c7f1d42d57ecbca0d51a3b54bc18b54
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-18 18:09:50 -0300

    :test_tube: test: refatora testes de mergeClass usando test.each
    Simplifica os testes ao utilizar casos parametrizados, reduzindo duplicação de código e melhorando a legibilidade
    das asserções.

--- src/components/ToDoItem/mergeClass.spec.ts
+++ src/components/ToDoItem/mergeClass.spec.ts
@@ -6,19 +6,17 @@ jest.mock('./todo-item.module.css', () => ({
 }))
 
 describe('./mergeClass.ts', () => {
-    test('deveria retornar a classe base do todo-item quando o item não estiver completo', () => {
-        const isItemCompleted = false
+    test.each([
+        { input: true, output: 'todo-item completed' },
+        { input: false, output: 'todo-item' },
+    ])(
+        'deveria retornar as classes $output quando o item completo for $input',
+        ({ input, output }) => {
+            const isItemCompleted = input
 
-        const styles = mergeClass(isItemCompleted)
+            const styles = mergeClass(isItemCompleted)
 
-        expect(styles).toBe('todo-item')
-    })
-
-    test('deveria retornar a classe base do todo-item e a classe completed quando o item estiver completo', () => {
-        const isItemCompleted = true
-
-        const styles = mergeClass(isItemCompleted)
-
-        expect(styles).toBe('todo-item completed')
-    })
+            expect(styles).toBe(output)
+        },
+    )
 })
