a2c6bffcc47f9d7f2d7b5335f418c9b29dee4244
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-20 18:26:04 -0300

    :test_tube: test: adiciona teste de renderização com itens
    Inclui um novo teste para garantir que o componente ToDoGroup renderiza corretamente a lista de todos quando recebe itens via props.

--- src/components/ToDoGroup/index.spec.tsx
+++ src/components/ToDoGroup/index.spec.tsx
@@ -10,4 +10,31 @@ describe('ToDoGroup', () => {
         expect(getByText('Teste')).toBeInTheDocument()
         expect(queryAllByRole('listitem')).toHaveLength(0)
     })
+
+    test('deveria renderizar os itens do grupo corretamente', () => {
+        const items = [
+            {
+                id: '9d7a',
+                description: 'Estudar React',
+                createdAt: '2025-08-22T10:00:00.000Z',
+                completed: false,
+            },
+            {
+                id: '2q8d',
+                description: 'Estudar Jest',
+                createdAt: '2025-08-22T11:00:00.000Z',
+                completed: true,
+            },
+        ]
+        const { getByText, queryAllByRole } = render(
+            <ToDoGroup todos={items} heading="Teste" />,
+        )
+
+        const todoItem1 = getByText('Estudar React')
+        const todoItem2 = getByText('Estudar Jest')
+
+        expect(queryAllByRole('listitem')).toHaveLength(2)
+        expect(todoItem1).toBeInTheDocument()
+        expect(todoItem2).toBeInTheDocument()
+    })
 })
